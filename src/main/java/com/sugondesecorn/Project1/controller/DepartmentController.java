c597ec58e4b4291fdbed905c74fc43eadfa03dde
hzlnutspread <smithkg293@gmail.com>
2022-09-13 12:01:29 +1200

    Add @Valid annotation to required fields. Add a logger. Add lombok to remove boilerplate

--- src/main/java/com/sugondesecorn/Project1/controller/DepartmentController.java
+++ src/main/java/com/sugondesecorn/Project1/controller/DepartmentController.java
@@ -2,9 +2,12 @@ package com.sugondesecorn.Project1.controller;
 
 import com.sugondesecorn.Project1.entity.Department;
 import com.sugondesecorn.Project1.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 // Tells the program that this will be your primary REST API controller
@@ -17,24 +20,31 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+//    From the slf4j package
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
 //    Need to pass in the entire json object too in the request body
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
     public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        LOGGER.info("Inside fetchDepartmentById of DepartmentController");
         return departmentService.fetchDepartmentById(departmentId);
     }
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
+        LOGGER.info("Inside deleteDepartmentById of DepartmentController");
         departmentService.deleteDepartmentById(departmentId);
         return "Department deleted successfully!";
     }
@@ -42,11 +52,13 @@ public class DepartmentController {
     @PutMapping("/departments/{id}")
     public Department updateDepartment(@PathVariable("id") Long departmentId,
                                        @RequestBody Department department) {
+        LOGGER.info("Inside updateDepartment of DepartmentController");
         return departmentService.updateDepartment(departmentId, department);
     }
 
     @GetMapping("/departments/name/{name}")
     public Department fetchDepartmentByName(@PathVariable("name") String departmentName) {
+        LOGGER.info("Inside fetchDepartmentByName of DepartmentController");
         return departmentService.fetchDepartmentByName(departmentName);
     }
 }
