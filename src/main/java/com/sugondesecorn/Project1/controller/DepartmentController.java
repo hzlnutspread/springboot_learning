e0762baaf338914a47f77b334e221a0b65045d81
hzlnutspread <smithkg293@gmail.com>
2022-09-17 16:54:45 +1200

    Implement error handling. Create Exception handler

--- src/main/java/com/sugondesecorn/Project1/controller/DepartmentController.java
+++ src/main/java/com/sugondesecorn/Project1/controller/DepartmentController.java
@@ -1,6 +1,7 @@
 package com.sugondesecorn.Project1.controller;
 
 import com.sugondesecorn.Project1.entity.Department;
+import com.sugondesecorn.Project1.error.DepartmentNotFoundException;
 import com.sugondesecorn.Project1.service.DepartmentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,8 @@ public class DepartmentController {
     private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
 
 //    Need to pass in the entire json object too in the request body
+//    The "/departments" is the default end point for this Post request method
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
         LOGGER.info("Inside saveDepartment of DepartmentController");
@@ -37,7 +40,7 @@ public class DepartmentController {
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
         LOGGER.info("Inside fetchDepartmentById of DepartmentController");
         return departmentService.fetchDepartmentById(departmentId);
     }
